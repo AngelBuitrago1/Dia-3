789015e3c0e28cc828fbb91f781c41951bea1bdc
AngelBuitrago1 <82422928+AngelBuitrago1@users.noreply.github.com>
2021-07-18 17:07:14 -0500

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,11 @@
+﻿c0 = int(input("Ingrese un número entero positivo: "))
+cont=0
+while c0 != 1:
+   if c0%2==0:
+     c0=c0/2
+     cont+=1
+   else:
+     c0=(3*c0)+1
+     cont+=1
+   print(str(int(c0)), end="\n")
+print('pasos=',cont)
