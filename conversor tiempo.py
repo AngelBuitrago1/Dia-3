8fe9f0b46605ec66b2367c2d3f7102c9f590c03e
AngelBuitrago1 <82422928+AngelBuitrago1@users.noreply.github.com>
2022-03-10 12:42:21 -0500

    Add files via upload
    Practice Python

---
+++ conversor tiempo.py
@@ -0,0 +1,10 @@
+﻿#este programa calcula los segundos en cierto número de horas determinadas 
+
+a = 2 # numero de horas
+segundos = 3600 # número de segundos en una hora
+
+print("Horas: ", a) #imprime el numero de horas
+print("Segundos en horas: ", a * segundos) # se imprime el numero de segundos en determinado numero de horas
+
+print("Adiós")
+#este el es fin del programa que calcula el numero de segundos en 2 horas
