9a139d213cf439b5452bdad596e3985656cb9f72
AngelBuitrago1 <82422928+AngelBuitrago1@users.noreply.github.com>
2022-03-11 10:14:16 -0500

    Add files via upload

---
+++ calculo evento.py
@@ -0,0 +1,11 @@
+﻿hora = int(input("Hora de inicio (horas): "))
+min = int(input("Minuto de inicio (minutos): "))
+dura = int(input("Duración del evento (minutos): "))
+
+# coloca tu código aqui
+a=min+dura
+b=a//60
+c=a%60
+hora += b
+hora=hora%24
+print("salida esperada: "+str(hora)+':'+str(c))
---
+++ impuestos.py
@@ -0,0 +1,15 @@
+﻿ingreso=float(input("Ingrese el ingreso anual:"))
+
+#
+# Coloca tu código aquí.
+#
+if (ingreso <=0):
+    impuesto=0.0
+elif (ingreso>0 and ingreso<=85528):
+    impuesto=(ingreso*.18)-566.2
+    if impuesto<0:
+        impuesto=0.0
+else:
+    impuesto=14839.2+((ingreso-85528)*.32)
+impuesto=round(impuesto, 0)
+print("El impuesto es: ", impuesto, "pesos")
